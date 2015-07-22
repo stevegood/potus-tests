abe9696eb83aca0b11a2d355f4925f0f63758964
SasidharAdapala <sasidharav.mca@gmail.com>
2015-07-23 00:04:15 +0530

    Updated the Functional Test Cases

--- src/test/groovy/com/herokuapp/potus/PresidentsSpec.groovy
+++ src/test/groovy/com/herokuapp/potus/PresidentsSpec.groovy
@@ -28,4 +28,76 @@ class PresidentsSpec extends GebReportingSpec {
     }
   }
 
+  def "Should test that API returns President Info(Name,Party) and the UI displays them based on the selection of the year"() {
+    when:
+    to SearchByYearPage
+    selectYear = '2002'
+    submitButton.Click()
+
+    then:
+    at IndexPage
+    def presidentInfo = api.getPresidentInfoByYear(2002)
+    def uiPresident = $("a[href='/president/presidentInfo._id']")
+      assert uiPresident
+      assert presidentInfo.name == uiPresident.text()
+      assert presidentInfo.party == uiPresident.party
+  }
+
+  def "Should test that API returns President Info(Name,Party) and the UI displays them based on search for First or Last name"() {
+    when:
+    to SearchByNamePage
+    searchname = "Abraham"
+    submitButton.Click()
+
+    then:
+    at IndexPage
+    def presidentInfo = api.getPresidentInfoByName("Abraham")
+    def uiPresident = $("a[href='/president/presidentInfo._id']")
+      assert uiPresident
+      assert presidentInfo.name == uiPresident.text()
+  }
+  
+  def "Should test that API returns Many President Info(Name,Party) and the UI displays them based on search for First or Last name"() {
+    when:
+    to SearchByNamePage
+    searchname = "Bush"
+    submitButton.Click()
+
+    then:
+    at IndexPage
+    def presidentInfo = api.getPresidentInfoByName("Bush")
+    def uiPresident = $("a[href='/president/presidentInfo._id']")
+      assert uiPresident
+      assert presidentInfo.name == uiPresident.text()
+      assert presidentInfo.size() == 2
+  }
+
+  def "Should test that API returns NO President Info(Name,Party) and the UI displays No Data  based on search for incorrect First or Last name"() {
+    when:
+    to SearchByNamePage
+    searchname = "XYZ"
+    submitButton.Click()
+
+    then:
+    at SearchByNamePage
+    def presidentInfo = api.getPresidentInfoByName("XYZ")
+    errors.size == 1
+    NoDataFoundError.present
+  }
+
+  def "Should test that API returns President Parties and the UI displays them as pie-chart"() {
+    when:
+    to IndexPage
+
+    then:
+    at IndexPage
+
+    when:
+    def apiPresidentsPartiesMap = api.listPresidentParties()
+
+    then:
+    assert apiPresidentsPartiesMap.collect{it.value.size()}
+    assert apiPresidentsPartiesMap.Federalist.size() == 1
+    assert apiPresidentsPartiesMap.Democrat.size() == 14
+  }  
 }
